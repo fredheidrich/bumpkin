0bda1362f80eb57de05c03f276fd404cc1df68f3
Fred <captain.hydd@gmail.com>
2021-09-15 09:29:24 +1000

    new file: adds setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+"""
+
+
+import sys
+import setuptools
+
+
+setuptools.setup(
+ name="standard-bumpkin",
+ py_modules="bumpkin",
+ author="Fred Heidrich",
+ classifiers = [
+     "Programming Language :: Python",
+     "Programming Language :: Python :: 3",
+     "Programming Language :: Python :: 3.7",
+     "Development Status :: 4 - Beta",
+     "Environment :: Other Environment",
+     "Intended Audience :: Developers",
+     "License :: OSI Approved :: MIT License",
+     "Operating System :: OS Independent",
+     "Topic :: Software Development :: Pre-processors",
+     "Topic :: Software Development :: Libraries :: Python Modules",
+     "Topic :: Software Development :: Code Generators",
+ ]
+)
+
