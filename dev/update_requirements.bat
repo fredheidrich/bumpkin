9e2200c083fc5416404ca8f3ff1292bad26cd064
Fred <captain.hydd@gmail.com>
2021-09-22 13:59:59 +1000

    distribute to pypi

--- dev/update_requirements.bat
+++ dev/update_requirements.bat
@@ -1,7 +1,10 @@
 
-@echo
+@echo off
 
-rem todo/fred: check if we run this from the root of the repo, otherwise error out
+if exist update_requirements.bat (
+ echo ERROR: please run this from the project root
+ exit /b 1
+)
 
 pip-compile --output-file=requirements.txt dev/requirements.in
 pip-compile --output-file=dev/requirements-dev.txt dev/requirements-dev.in
